be2bc4bdd1f435fa286a0df8944978e3b84318c2
MatheusRamos90 <matheus.hrs@live.com>
2021-01-28 11:17:40 -0300

    Add main class

---
+++ src/br/com/jogogourmet/JogoGourmetBootstrap.java
@@ -0,0 +1,11 @@
+package br.com.jogogourmet;
+
+import br.com.jogogourmet.panels.JogoGourmetPanel;
+
+public class JogoGourmetBootstrap {
+	
+	public static void main(String[] args) {
+		new JogoGourmetPanel();
+	}
+
+}
